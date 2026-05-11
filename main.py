9fa8690fe5dd9ec9cea3f260af37e95942aaec73
Naviya Manoj <48naviya@gmail.com>
2026-05-12 03:00:02 +0530

    feat: test auth routes working

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from routes.auth import router as auth_router
+from database import engine
+from models import Base
 
 app=FastAPI()
 
 @app.get("/")
 def root():
     return {"Message" : "DSA Chatbot API running"}
+
+app.include_router(auth_router, prefix="/auth")
+
+Base.metadata.create_all(bind=engine)
--- routes/auth.py
+++ routes/auth.py
@@ -17,4 +17,12 @@ def register(email, password, db: Session=Depends(get_db)):
         db.commit()
         return {"message" : "user registered successfully"}
         
+@router.post("/login")
+def login(email, password, db: Session=Depends(get_db)):
+    user=db.query(User).filter(User.email==email).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    elif not verify_password(password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Invalid Credentials")
+    return create_access_token({"sub":str(user.id)})
 
